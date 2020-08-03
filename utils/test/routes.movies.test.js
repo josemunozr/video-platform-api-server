ef6c9d5d9929be93779554fe82bee6adec2669c8
José Muñoz Ruiz <jozeepp@gmail.com>
2020-08-03 07:58:00 -0400

    initial test with mocha to route movies

---
+++ utils/test/routes.movies.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const proxyquire = require('proxyquire');
+
+const { moviesMock, MoviesServicesMosk } = require('../mocks/moviesMock');
+const testServer = require('../testServer');
+
+describe('routes - movies', () => {
+  const route = proxyquire('../../router/movies', {
+    '../services/movies': MoviesServicesMosk,
+  });
+
+  const request = testServer(route);
+  describe('GET /movies', () => {
+    it('should response with status 200', (done) => {
+      request.get('/api/movies').expect(200, done);
+    });
+
+    it('should response the list of movies', (done) => {
+      request.get('/api/movies').end((erro, res) => {
+        assert.deepEqual(res.body, {
+          data: moviesMock,
+          message: 'movies listed',
+        });
+        done();
+      });
+    });
+  });
+});
