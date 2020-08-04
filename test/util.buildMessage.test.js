712bfbf4adc7b83b227571ceb8e5bf9b500685c4
José Muñoz Ruiz <jozeepp@gmail.com>
2020-08-04 08:23:37 -0400

    TDD test examen with util.buildMessage.test.js

---
+++ test/util.buildMessage.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const buildMessage = require('../utils/buildMessage');
+
+describe.only('utils - buildMessage', () => {
+  describe('when receives an entity and a action', () => {
+    it('should return then respective message', () => {
+      const result = buildMessage('movie', 'create');
+      const expected = 'movie created';
+      assert.strictEqual(result, expected);
+    });
+  });
+  
+  describe('when receives an entity and action and is a list', () => {
+    it('should return then respective message with the entity in plural', () => {
+      const result = buildMessage('movie', 'list');
+      const expected = 'movies listed';
+      assert.strictEqual(result, expected);
+    });
+  });
+
+});
