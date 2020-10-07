0a4d235e6b144ef9327082659e483d347b71219f
José Muñoz Ruiz <jozeepp@gmail.com>
2020-10-07 08:00:51 -0300

    user schema and userService created

---
+++ services/users.js
@@ -0,0 +1,29 @@
+const MongoLib = require('../lib/mongo');
+const bcrypt = require('bcrypt');
+
+class UsersService {
+  constructor() {
+    this.mongoDB = new MongoLib();
+    this.collection = 'users';
+  }
+
+  async getUser({ email }) {
+    const [user] = await this.mongoDB.getAll(this.collection, { email });
+    return user;
+  }
+
+  async createUser({ user }) {
+    const { name, email, password } = user;
+    const hashedPassword = await bcrypt(password, 10);
+
+    const createUserID = await this.mongoDB.create(this.collection, {
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return createUserID;
+  }
+}
+
+module.exports = UsersService;
