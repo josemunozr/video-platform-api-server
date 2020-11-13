6385681459cc2b15219dd77c3b38c4ba5e1e4fda
José Muñoz Ruiz <jozeepp@gmail.com>
2020-11-13 07:08:35 -0300

    helmet middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config/index');
@@ -11,6 +12,7 @@ const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 //Body parser
 app.use(express.json());
+app.use(helmet())
 
 //routes
 authApi(app);
